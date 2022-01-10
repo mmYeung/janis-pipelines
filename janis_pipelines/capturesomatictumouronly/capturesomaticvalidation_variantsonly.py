4e4e83a63659495947a1521f0fb79b5b06a24c7f
Miriam Yeung <miriam.yeung@petermac.org>
2022-01-10 13:18:47 +1100

    Initial commit, Validation specifc versions of the Hybrid capture pipeline

---
+++ janis_pipelines/capturesomatictumouronly/capturesomaticvalidation_variantsonly.py
@@ -0,0 +1,244 @@
+from janis_core import Int, Float, Directory, StringFormatter
+from janis_bioinformatics.data_types import Bed, Vcf, VcfTabix
+
+
+from janis_pipelines.capturesomatictumouronly_gatk.capturesomaticvalidationgatk_variantsonly import (
+    CaptureSomaticValidationGATKVariantsOnly,
+)
+
+from janis_bioinformatics.tools.variantcallers import (
+    VardictGermlineVariantCaller,
+    VarscanGermlineCNSVariantCaller,
+    IlluminaSomaticPiscesVariantCallerTumourOnlyTargeted_5_2_10_49,
+)
+
+from janis_bioinformatics.tools.dawson import GenerateChromosomeIntervalsFromBed
+
+from janis_bioinformatics.tools.pmac import (
+    CombineVariants_0_0_8,
+    AddBamStatsGermline_0_1_0,
+    GenerateVardictHeaderLines,
+)
+
+from janis_bioinformatics.tools.bcftools import BcfToolsSort_1_9
+
+from janis_unix.tools import UncompressArchive
+
+from janis_bioinformatics.tools.htslib import BGZipLatest, TabixLatest
+
+
+class CaptureSomaticValidationMultiCallersVariantsOnly(
+    CaptureSomaticValidationGATKVariantsOnly
+):
+    def id(self):
+        return "CaptureSomaticValidationMultiCallersVariantsOnly"
+
+    def friendly_name(self):
+        return "Capture Somatic Tumour Only (Multi callers) [VARIANTS only]"
+
+    def version(self):
+        return "1.4.0"
+
+    def constructor(self):
+        self.add_inputs()
+
+        self.add_gatk_variantcaller(tumour_bam_source=self.bam)
+        self.add_vardict(bam_source=self.bam)
+        self.add_varscan2(bam_source=self.bam)
+        self.add_pisces(bam_source=self.bam)
+
+        self.add_combine_variants(bam_source=self.bam)
+
+    def add_inputs_for_reference(self):
+        super().add_inputs_for_reference()
+        self.input("referenceFolder", Directory())
+
+    def add_inputs_for_intervals(self):
+        super().add_inputs_for_intervals()
+        self.input("intervals", Bed())
+
+    def add_inputs_for_vc(self):
+        # General parameters
+        self.input("minVaf", Float())
+        self.input("minMQ", Int())
+        self.input("minAD", Int())
+        self.input("minDP", Int())
+        self.input("minBQ", Int())
+        # Varscan2
+        self.input("pileupMaxDepth", Int(), default=20000)
+        self.input("pileupMinBQ", Int())
+        self.input("varscanPval", Float(), default=0.0001)
+
+        # Pisces
+        self.input("piscesVCminVQ", Int())
+        self.input("piscesVQRminVQ", Int())
+
+    def add_inputs(self):
+        super().add_inputs()
+        self.add_inputs_for_vc()
+
+    def add_vardict(self, bam_source):
+        self.step(
+            "generate_vardict_headerlines",
+            GenerateVardictHeaderLines(reference=self.reference),
+        )
+
+        self.step(
+            "vc_vardict",
+            VardictGermlineVariantCaller(
+                bam=bam_source,
+                sample_name=self.sample_name,
+                intervals=self.intervals,
+                header_lines=self.generate_vardict_headerlines.out,
+                reference=self.reference,
+                allele_freq_threshold=self.minVaf,
+                minMappingQual=self.minMQ,
+                sv=False,
+            ),
+        )
+
+        self.output(
+            "out_variants_vardict",
+            source=self.vc_vardict.out,
+            output_folder="variants",
+            output_name=StringFormatter(
+                "{samplename}.vardict.recode.vcf", samplename=self.sample_name
+            ),
+        )
+        self.output(
+            "variants_vardict",
+            source=self.vc_vardict.variants,
+            output_folder=["variants", "unfiltered"],
+            output_name=StringFormatter(
+                "{samplename}.vardict.vcf", samplename=self.sample_name
+            ),
+        )
+
+    def add_varscan2(self, bam_source):
+        self.step(
+            "vc_varscan2",
+            VarscanGermlineCNSVariantCaller(
+                sample_name=self.sample_name,
+                bam=bam_source,
+                reference=self.reference,
+                maxDepth=self.pileupMaxDepth,
+                minBQ=self.minBQ,
+                minDP=self.minDP,
+                minAD=self.minAD,
+                minVaf=self.minVaf,
+                pval=self.varscanPval,
+            ),
+        )
+
+        self.output(
+            "out_variants_varscan2",
+            source=self.vc_varscan2.out,
+            output_folder="variants",
+            output_name=StringFormatter(
+                "{samplename}.varscan.recode.vcf", samplename=self.sample_name
+            ),
+        )
+        self.output(
+            "variants_varscan2",
+            source=self.vc_varscan2.variants,
+            output_folder=["variants", "unfiltered"],
+            output_name=StringFormatter(
+                "{samplename}.varscan.vcf", samplename=self.sample_name
+            ),
+        )
+
+    def add_pisces(self, bam_source):
+        self.step(
+            "vc_pisces",
+            IlluminaSomaticPiscesVariantCallerTumourOnlyTargeted_5_2_10_49(
+                bam=bam_source,
+                sample_name=self.sample_name,
+                referenceFolder=self.referenceFolder,
+                PON=self.panel_of_normals,
+                intervals=self.intervals,
+                minBQ=self.minBQ,
+                VCminVQ=self.piscesVCminVQ,
+                VQRminVQ=self.piscesVQRminVQ,
+            ),
+        )
+
+        self.output(
+            "piscesBam",
+            source=self.vc_pisces.out_bam,
+            output_folder="Bam",
+            output_name=StringFormatter(
+                "{samplename}.pisces.bam", samplename=self.sample_name
+            ),
+        )
+        self.output(
+            "out_variants_pisces",
+            source=self.vc_pisces.out,
+            output_folder="variants",
+            output_name=StringFormatter(
+                "{samplename}.pisces.recode.vcf", samplename=self.sample_name
+            ),
+        )
+        self.output(
+            "variants_pisces",
+            source=self.vc_pisces.variants,
+            output_folder=["variants", "unfiltered"],
+            output_name=StringFormatter(
+                "{samplename}.pisces.vcf", samplename=self.sample_name
+            ),
+        )
+
+    def add_inputs_for_intervals(self):
+        self.input("intervals", Bed())
+
+    def add_combine_variants(self, bam_source):
+        self.step(
+            "combine_variants",
+            CombineVariants_0_0_8(
+                vcfs=[
+                    self.vc_gatk_filterpass.out,
+                    self.vc_pisces.out,
+                    self.vc_varscan2.out,
+                    self.vc_vardict.out,
+                ],
+                type="germline",
+                columns=["AC", "AN", "AF", "AD", "DP", "GT"],
+            ),
+        )
+
+        self.step(
+            "combined_sort", BcfToolsSort_1_9(vcf=self.combine_variants.out)
+        )
+
+        self.step(
+            "combined_uncompress",
+            UncompressArchive(file=self.combined_sort.out),
+        )
+
+        self.step(
+            "combined_addbamstats",
+            AddBamStatsGermline_0_1_0(
+                bam=bam_source,
+                vcf=self.combined_uncompress.out.as_type(Vcf),
+                reference=self.reference,
+            ),
+        )
+
+        self.step(
+            "compress_combined_addbamstats",
+            BGZipLatest(file=self.combined_addbamstats),
+        )
+
+        self.step(
+            "tabixvcf", TabixLatest(inp=self.compress_combined_addbamstats.out)
+        )
+
+        self.output(
+            "out_variants",
+            source=self.tabixvcf.out,
+            output_folder=["variants"],
+            output_name=StringFormatter(
+                "{samplename}.combined", samplename=self.sample_name
+            ),
+            doc="Combined variants from all 4 variant callers",
+        )
+
---
+++ janis_pipelines/capturesomatictumouronly/capturesomaticvalidationumi.py
@@ -0,0 +1,171 @@
+from typing import Optional, List
+from janis_bioinformatics.data_types.fastq import FastqPair
+from janis_core import String, Array, File, WorkflowBuilder
+
+from janis_bioinformatics.data_types import FastqGzPair, FastaWithDict
+
+from janis_bioinformatics.tools.agent import AgentTrimmer_2_0_2
+
+from janis_bioinformatics.tools.babrahambioinformatics import FastQC_0_11_8
+
+from janis_bioinformatics.tools.pmac import ParseFastqcAdaptors
+
+from janis_bioinformatics.tools.bwakit import BwaPostAltAlignerUMI
+
+from janis_bioinformatics.tools.common import MergeAndMarkBamsUMI_4_1_2
+
+from janis_pipelines.capturesomatictumouronly.capturesomatictumouronly_variantsonly import (
+    CaptureSomaticTumourOnlyMultiCallersVariantsOnly,
+)
+
+
+class CaptureSomaticTumourOnlyUMI(
+    CaptureSomaticTumourOnlyMultiCallersVariantsOnly
+):
+    def id(self):
+        return "CaptureSomaticTumourOnlyUMI"
+
+    def friendly_name(self):
+        return "Capture Somatic Tumour Only with UMIs (Multi callers)"
+
+    def version(self):
+        return "0.0.0"
+
+    def constructor(self):
+        self.add_inputs()
+
+        self.step(
+            "agenttrim",
+            self.umi_trimmer_subworkflow(
+                fastqPair=self.reads, agentlibrary=self.agentlibrary
+            ),
+            scatter=["fastqPair"],
+        )
+
+        self.step(
+            "fastqc", FastQC_0_11_8(reads=self.agenttrim.out), scatter="reads"
+        )
+
+        self.step(
+            "getfastqc_adapters",
+            ParseFastqcAdaptors(
+                fastqc_datafiles=self.fastqc.datafile,
+                cutadapt_adaptors_lookup=self.cutadapt_adapters,
+            ),
+            scatter="fastqc_datafiles",
+        )
+
+        self.add_preprocessing()
+
+        self.step(
+            "callers",
+            CaptureSomaticTumourOnlyMultiCallersVariantsOnly(
+                bam=self.merge_and_mark.out,
+                sample_name=self.sample_name,
+                reference=self.reference,
+                referenceFolder=self.referenceFolder,
+                intervals=self.intervals,
+                minVaf=self.minVaf,
+                minMQ=self.minMQ,
+                minAD=self.minAD,
+                minDP=self.minDP,
+                minBQ=self.minBQ,
+                pileupMaxDepth=self.pileupMaxDepth,
+                pileupMinBQ=self.pileupMinBQ,
+                varscanPval=self.varscanPval,
+                piscesVCminVQ=self.piscesVCminVQ,
+                piscesVQRminVQ=self.piscesVQRminVQ,
+                snps_dbsnp=self.snps_dbsnp,
+                snps_1000gp=self.snps_1000gp,
+                known_indels=self.known_indels,
+                mills_indels=self.mills_indels,
+                gnomad=self.gnomad,
+                panel_of_normals=self.panel_of_normals,
+            ),
+        )
+
+        # self.output("out", self.callers.out_variants)
+
+    def add_inputs(self):
+        self.input("reads", Array(FastqGzPair))
+        self.input("sample_name", String())
+        self.input("referenceAlt", File())
+        # For Agent Trimmer
+        self.input("agentlibrary", String())
+        # For CutAdapt
+        self.input("cutadapt_adapters", File(optional=True))
+        self.add_inputs_for_reference()
+        self.add_inputs_for_configuration()
+        self.add_inputs_for_intervals()
+        self.add_inputs_for_vc()
+
+    @staticmethod
+    def umi_trimmer_subworkflow(**connections):
+        w = WorkflowBuilder("umi_trimmer_subworkflow")
+
+        ## Inputs
+        w.input("fastqPair", FastqGzPair())
+        w.input("agentlibrary", String())
+
+        w.step(
+            "agenttrimsub",
+            AgentTrimmer_2_0_2(
+                read1=w.fastqPair[0],
+                read2=w.fastqPair[1],
+                outdir=".",
+                library=w.agentlibrary,
+                agentVersion="2.0.2",
+            ),
+        )
+
+        w.output("out", source=w.agenttrimsub.out)
+
+        return w(**connections)
+
+    def add_preprocessing(self):
+
+        sub_inputs = {
+            "reference": self.reference,
+            "referenceAlt": self.referenceAlt,
+            "cutadapt_adapter": self.getfastqc_adapters,
+            "cutadapt_removeMiddle3Adapter": self.getfastqc_adapters,
+        }
+
+        ## STEPS
+        self.step(
+            "alignUMI",
+            BwaPostAltAlignerUMI(
+                fastq=self.agenttrim.out,
+                sample_name=self.sample_name,
+                addumis=True,
+                **sub_inputs,
+            ),
+            scatter=[
+                "fastq",
+                "cutadapt_adapter",
+                "cutadapt_removeMiddle3Adapter",
+            ],
+        )
+
+        self.step(
+            "merge_and_mark",
+            MergeAndMarkBamsUMI_4_1_2(
+                bams=self.alignUMI.out, sample_name=self.sample_name
+            ),
+        )
+
+        ## OUTPUTS
+        self.output("out_bam", source=self.merge_and_mark.out)
+        self.output(
+            "umimetrics",
+            source=self.merge_and_mark.umimetrics,
+            output_folder=["stats"],
+            output_name=self.sample_name + "umimetrics.txt",
+        )
+        self.output(
+            "metrics",
+            source=self.merge_and_mark.metrics,
+            output_folder=["stats"],
+            output_name=self.sample_name + "metrics.txt",
+        )
+        self.output("out_fastqc_reports", source=self.fastqc.out)
