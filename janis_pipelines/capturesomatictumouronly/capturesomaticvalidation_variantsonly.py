5b7a8b9eb663c7676e3e3805585ac281b2d1db5b
Miriam Yeung <miriam.yeung@petermac.org>
2022-03-22 10:21:53 +1100

    Changing which pipeline this inherits from

--- janis_pipelines/capturesomatictumouronly/capturesomaticvalidation_variantsonly.py
+++ janis_pipelines/capturesomatictumouronly/capturesomaticvalidation_variantsonly.py
@@ -1,37 +1,17 @@
-from janis_core import Int, Float, Directory, File, StringFormatter
-from janis_bioinformatics.data_types import Bed, Vcf, VcfTabix
+from janis_core import StringFormatter
 
 
-from janis_pipelines.capturesomatictumouronly_gatk.capturesomaticvalidationgatk_variantsonly import (
-    CaptureSomaticValidationGATKVariantsOnly,
+from janis_pipelines.capturesomatictumouronly.capturesomatictumouronly_variantsonly import (
+    CaptureSomaticTumourOnlyMultiCallersVariantsOnly,
 )
 
 from janis_bioinformatics.tools.variantcallers import (
-    VardictGermlineVariantCaller,
-    VarscanGermlineCNSVariantCaller,
     IlluminaSomaticPiscesVariantCallerTumourOnlyTargetedNoPON_5_2_10_49,
 )
 
-from janis_bioinformatics.tools.dawson import (
-    GenerateChromosomeIntervalsFromBed,
-    GenerateVarscan2HeaderLines,
-)
-
-from janis_bioinformatics.tools.pmac import (
-    CombineVariants_0_0_8,
-    AddBamStatsGermline_0_1_0,
-    GenerateVardictHeaderLines,
-)
-
-from janis_bioinformatics.tools.bcftools import BcfToolsSort_1_9
-
-from janis_unix.tools import UncompressArchive
-
-from janis_bioinformatics.tools.htslib import BGZipLatest, TabixLatest
-
 
 class CaptureSomaticValidationMultiCallersVariantsOnly(
-    CaptureSomaticValidationGATKVariantsOnly
+    CaptureSomaticTumourOnlyMultiCallersVariantsOnly
 ):
     def id(self):
         return "CaptureSomaticValidationMultiCallersVariantsOnly"
@@ -41,147 +21,6 @@ class CaptureSomaticValidationMultiCallersVariantsOnly(
 
     def version(self):
         return "1.4.0"
-
-    def constructor(self):
-        self.add_inputs()
-
-        self.add_gatk_variantcaller(tumour_bam_source=self.bam)
-        ## Adding outputs from GATK for easier referencing in following pipelines
-        self.output(
-            "out_variants_gatk",
-            source=self.vc_gatk_filterpass.out,
-            output_folder=["variants"],
-            output_name=StringFormatter(
-                "{samplename}.gatk.recode", samplename=self.sample_name
-            ),
-        )
-        self.output(
-            "variants_gatk",
-            source=self.vc_gatk_sort.out,
-            output_folder=["variants", "unfiltered"],
-            output_name=StringFormatter(
-                "{samplename}.gatk", samplename=self.sample_name
-            ),
-        )
-        self.output(
-            "gatk_bam",
-            source=self.vc_gatk.out_bam,
-            output_folder=["Bam"],
-            output_name=StringFormatter(
-                "{samplename}.gatk", samplename=self.sample_name
-            ),
-        )
-
-        self.add_vardict(bam_source=self.bam)
-        self.add_varscan2(bam_source=self.bam)
-        self.add_pisces(bam_source=self.bam)
-
-        self.add_combine_variants(bam_source=self.bam)
-
-    def add_inputs_for_reference(self):
-        super().add_inputs_for_reference()
-        self.input("referenceFolder", Directory())
-
-    def add_inputs_for_intervals(self):
-        super().add_inputs_for_intervals()
-        self.input("intervals", Bed())
-
-    def add_inputs_for_vc(self):
-        # General parameters
-        self.input("minVaf", Float())
-        self.input("minMQ", Int())
-        self.input("minAD", Int())
-        self.input("minDP", Int())
-        self.input("minBQ", Int())
-        # Varscan2
-        self.input("pileupMaxDepth", Int(), default=20000)
-        self.input("pileupMinBQ", Int())
-        self.input("varscanPval", Float(), default=0.0001)
-
-        # Pisces
-        self.input("piscesVCminVQ", Int())
-        self.input("piscesVQRminVQ", Int())
-        self.input("pisces_awk_script", File())
-        self.input("pisces_noise_level", Int(optional=True))
-
-    def add_inputs(self):
-        super().add_inputs()
-        self.add_inputs_for_vc()
-
-    def add_vardict(self, bam_source):
-        self.step(
-            "generate_vardict_headerlines",
-            GenerateVardictHeaderLines(reference=self.reference),
-        )
-
-        self.step(
-            "vc_vardict",
-            VardictGermlineVariantCaller(
-                bam=bam_source,
-                sample_name=self.sample_name,
-                intervals=self.intervals,
-                header_lines=self.generate_vardict_headerlines.out,
-                reference=self.reference,
-                allele_freq_threshold=self.minVaf,
-                minMappingQual=self.minMQ,
-                sv=True,
-            ),
-        )
-
-        self.output(
-            "out_variants_vardict",
-            source=self.vc_vardict.out,
-            output_folder="variants",
-            output_name=StringFormatter(
-                "{samplename}.vardict.recode.vcf", samplename=self.sample_name
-            ),
-        )
-        self.output(
-            "variants_vardict",
-            source=self.vc_vardict.variants,
-            output_folder=["variants", "unfiltered"],
-            output_name=StringFormatter(
-                "{samplename}.vardict.vcf", samplename=self.sample_name
-            ),
-        )
-
-    def add_varscan2(self, bam_source):
-        self.step(
-            "generate_varscan2_headerlines",
-            GenerateVarscan2HeaderLines(reference=self.reference),
-        )
-        self.step(
-            "vc_varscan2",
-            VarscanGermlineCNSVariantCaller(
-                sample_name=self.sample_name,
-                bam=bam_source,
-                reference=self.reference,
-                maxDepth=self.pileupMaxDepth,
-                minBQ=self.minBQ,
-                minDP=self.minDP,
-                minAD=self.minAD,
-                minVaf=self.minVaf,
-                pval=self.varscanPval,
-                header_lines=self.generate_varscan2_headerlines.out,
-            ),
-        )
-
-        self.output(
-            "out_variants_varscan2",
-            source=self.vc_varscan2.out,
-            output_folder="variants",
-            output_name=StringFormatter(
-                "{samplename}.varscan.recode.vcf", samplename=self.sample_name
-            ),
-        )
-        self.output(
-            "variants_varscan2",
-            source=self.vc_varscan2.variants,
-            output_folder=["variants", "unfiltered"],
-            output_name=StringFormatter(
-                "{samplename}.varscan.vcf", samplename=self.sample_name
-            ),
-        )
 
     def add_pisces(self, bam_source):
         self.step(
@@ -193,10 +32,10 @@ class CaptureSomaticValidationMultiCallersVariantsOnly(
                 intervals=self.intervals,
                 minBQ=self.minBQ,
                 minMQ=self.minMQ,
-                minDP = self.minDP,
-                minVAF=self.minVAF,
+                minDP=self.minDP,
+                minVAF=self.minVaf,
                 VCminVQ=self.piscesVCminVQ,
-                noise_level=self.pisces_noise_level
+                noise_level=self.pisces_noise_level,
                 VQRminVQ=self.piscesVQRminVQ,
                 pisces_awk_script=self.pisces_awk_script,
             ),
@@ -225,60 +64,5 @@ class CaptureSomaticValidationMultiCallersVariantsOnly(
             output_name=StringFormatter(
                 "{samplename}.pisces.vcf", samplename=self.sample_name
             ),
-        )
-
-    def add_inputs_for_intervals(self):
-        self.input("intervals", Bed())
-
-    def add_combine_variants(self, bam_source):
-        self.step(
-            "combine_variants",
-            CombineVariants_0_0_8(
-                vcfs=[
-                    self.vc_gatk_filterpass.out,
-                    self.vc_pisces.out,
-                    self.vc_varscan2.out,
-                    self.vc_vardict.out,
-                ],
-                type="germline",
-                columns=["AC", "AN", "AF", "AD", "DP", "GT"],
-            ),
-        )
-
-        self.step(
-            "combined_sort", BcfToolsSort_1_9(vcf=self.combine_variants.out)
-        )
-
-        self.step(
-            "combined_uncompress",
-            UncompressArchive(file=self.combined_sort.out),
-        )
-
-        self.step(
-            "combined_addbamstats",
-            AddBamStatsGermline_0_1_0(
-                bam=bam_source,
-                vcf=self.combined_uncompress.out.as_type(Vcf),
-                reference=self.reference,
-            ),
-        )
-
-        self.step(
-            "compress_combined_addbamstats",
-            BGZipLatest(file=self.combined_addbamstats),
-        )
-
-        self.step(
-            "tabixvcf", TabixLatest(inp=self.compress_combined_addbamstats.out)
-        )
-
-        self.output(
-            "out_variants",
-            source=self.tabixvcf.out,
-            output_folder=["variants"],
-            output_name=StringFormatter(
-                "{samplename}.combined.vcf", samplename=self.sample_name
-            ),
-            doc="Combined variants from all 4 variant callers",
         )
 
