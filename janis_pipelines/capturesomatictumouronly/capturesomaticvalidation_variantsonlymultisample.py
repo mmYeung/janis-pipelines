24c40eba88bb041eeb07fe35d6b05ad7a0591f01
Miriam Yeung <miriam.yeung@petermac.org>
2022-05-02 12:07:13 +1000

    initial commit, multisample version of capture validation variant calling

---
+++ janis_pipelines/capturesomatictumouronly/capturesomaticvalidation_variantsonlymultisample.py
@@ -0,0 +1,100 @@
+from janis_core import (
+    Int,
+    Float,
+    String,
+    Array,
+    Directory,
+    File,
+    StringFormatter,
+    ScatterMethod,
+    ScatterDescription,
+)
+
+from janis_bioinformatics.tools import BioinformaticsWorkflow
+
+from janis_bioinformatics.data_types import BamBai, Bed, VcfTabix, FastaWithDict
+
+from janis_pipelines.capturesomatictumouronly.capturesomaticvalidation_variantsonly import (
+    CaptureSomaticValidationMultiCallersVariantsOnly,
+)
+
+
+class CaptureSomaticValidationVariantsOnlyMultiSample(BioinformaticsWorkflow):
+    def id(self):
+        return "CaptureSomaticValidationVariantsOnlyMultiSample"
+
+    def friendly_name(self):
+        return "Capture Somatic Validation Variants Only (MultiSample)"
+
+    def version(self):
+        return "0.0.0"
+
+    def constructor(self):
+        ##Inputs
+        self.input("bam", Array(BamBai()))
+        self.input("sample_name", Array(String()))
+        ## Intervals
+        self.input("intervals", Bed())
+        # References
+        self.input("reference", FastaWithDict())
+        self.input("reference_alt", File())
+        self.input("reference_folder", Directory())
+        # GATK references
+        self.input("snps_dbsnp", VcfTabix())
+        self.input("snps_1000gp", VcfTabix())
+        self.input("known_indels", VcfTabix())
+        self.input("mills_indels", VcfTabix())
+        self.input("gnomad", VcfTabix())
+        # Variant Callers General
+        self.input("min_bq", Int())
+        self.input("min_mq", Int())
+        self.input("min_ad", Int())
+        self.input("min_dp", Int())
+        self.input("min_vaf", Float())
+        ## Varscan2
+        self.input("pileup_min_bq", Int())
+        self.input("pileup_max_depth", Int(), default=20000)
+        self.input("varscan_pval", Float(), default=0.0001)
+        ## Pisces
+        self.input("pisces_ploidy", String(optional=True))
+        self.input("pisces_vc_min_vq", Int())
+        self.input("pisces_vqr_min_vq", Int())
+        self.input("pisces_noise_level", Int(optional=True))
+        self.input("pisces_awk_script", File())
+
+        self.step(
+            "capture_variantcalling",
+            CaptureSomaticValidationMultiCallersVariantsOnly(
+                bam=self.bam,
+                sample_name=self.sample_name,
+                intervals=self.intervals,
+                reference=self.reference,
+                reference_alt=self.reference_alt,
+                reference_folder=self.reference_folder,
+                known_indels=self.known_indels,
+                mills_indels=self.mills_indels,
+                snps_1000gp=self.snps_1000gp,
+                snps_dbsnp=self.snps_dbsnp,
+                gnomad=self.gnomad,
+                agentlibrary=self.agentlibrary,
+                min_bq=self.min_bq,
+                min_mq=self.min_mq,
+                min_ad=self.min_ad,
+                min_dp=self.min_dp,
+                min_vaf=self.min_vaf,
+                pileup_min_bq=self.pileup_min_bq,
+                pileup_max_depth=self.pileup_max_depth,
+                varscan_pval=self.varscan_pval,
+                pisces_ploidy=self.pisces_ploidy,
+                pisces_vc_min_vq=self.pisces_vc_min_vq,
+                pisces_vqr_min_vq=self.pisces_vqr_min_vq,
+                pisces_noise_level=self.pisces_noise_level,
+                pisces_awk_script=self.pisces_awk_script,
+            ),
+            scatter=ScatterDescription(
+                ["reads", "sample_name"],
+                method=ScatterMethod.dot,
+                labels=self.sample_name,
+            ),
+        )
+        self.capture_outputs_from_step(self.capture_variantcalling)
